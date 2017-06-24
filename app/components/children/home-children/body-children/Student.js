c87144dac51b56b5dd247840471ecc57ab608693
Nicholas Xanttopoulos <nxanttopoulos@gmail.com>
2017-06-24 12:11:49 -0400

    Student Component Complete

---
+++ app/components/children/home-children/body-children/Student.js
@@ -0,0 +1,37 @@
+import React from 'react'
+class Student extends React.Component {
+  Constructor(){
+    super();
+    this.state = {   
+      name:"Name",
+      img:"http:www.img.jpg"
+    };
+  },
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <div>
+                <img alt="Student Image" src={this.props.img} width="100%" />
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+              <h1>{this.props.name}</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <h3> Followers </h3>
+          </div>
+          <div className="col-md-6">
+            <h3> Repos </h3>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Student
