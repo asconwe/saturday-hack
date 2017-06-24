be75c23159f7fbbb7c358de3e3ac1f69a7bf72a7
Damien <dasseya1@gmail.com>
2017-06-24 11:52:22 -0400

    Grabbed app id

--- app/App.js
+++ app/App.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 const appRoot = document.getElementById('app');
 const App = (props) => <h1>Github Respository Viewer</h1>;
 
-ReactDOM.render(<App/>, appRoot);
+ReactDOM.render(<App/>, document.querySelector("#app"));
