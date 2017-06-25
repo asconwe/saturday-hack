18d45bfd97e61248b59a1b88a744158833655ac6
Damien <dasseya1@gmail.com>
2017-06-25 16:06:51 -0400

    Displayed name and picture for user profile

---
+++ app/components/children/profile-children/UserBio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class UserBio extends React.Component {
+    render() {
+        return (
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img src={this.props.avatar_url} className="img img-responsive" />
+ 
+                            
+                        </div>
+                        <div className="col-md-3">
+                            <p>Name: {this.props.login}</p>
+                        </div>
+                    </div>
+        
+            );          
+        }
+
+    }
